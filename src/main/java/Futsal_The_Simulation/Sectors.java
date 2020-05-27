0f63a0efbd030cf5ab171e39152a1c7ccac28136
SaltyPuccini <kkajetanwojciechowski@gmail.com>
2020-05-27 17:44:06 +0200

    Field generator rdy to use

--- src/main/java/Futsal_The_Simulation/Sectors.java
+++ src/main/java/Futsal_The_Simulation/Sectors.java
@@ -1,29 +1,18 @@
 package Futsal_The_Simulation;
 
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 
 public class Sectors {
 
-    int index;
-    ArrayList<Integer> arrayOfSectorsID = new ArrayList();
-    void loadSectors() throws IOException, JSONException {
+    int id;
+    int capacity;
+    JSONArray connectedSectors;
 
-        String content = new String(Files.readAllBytes(Paths.get("src/main/resources/sektory.json")));
-        JSONArray arr = new JSONArray(content);
-        JSONObject obj = arr.getJSONObject(0);
-        int id = obj.getInt("id");
-        System.out.println(id);
-
-        for(int i=0; i<arr.length(); i++)
-        {
-            this.arrayOfSectorsID.add(id);
-        }
+    Sectors(int id, int capacity, JSONArray connectedSectors){
+        this.id=id;
+        this.capacity=capacity;
+        this.connectedSectors=connectedSectors;
     }
+
+
 }
--- src/main/java/Futsal_The_Simulation/Test.java
+++ src/main/java/Futsal_The_Simulation/Test.java
@@ -1,21 +1,14 @@
 package Futsal_The_Simulation;
 
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class Test {
 
     public static void main(String[] args) throws JSONException, IOException {
 
-        String content = new String(Files.readAllBytes(Paths.get("src/main/resources/sektory.json")));
-        JSONArray arr = new JSONArray(content);
-        JSONObject obj = arr.getJSONObject(0);
-        int id = obj.getInt("id");
-        System.out.println(id);
+        FieldGenerator field = new FieldGenerator();
+        field.loadSectors();
     }
 }
