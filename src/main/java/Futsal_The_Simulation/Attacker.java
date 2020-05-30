5925cd8c0844fd4147e7f6eca5839c432b0a3d9f
SaltyPuccini <kkajetanwojciechowski@gmail.com>
2020-05-30 18:06:51 +0200

    int the middle of adding shooting method

--- src/main/java/Futsal_The_Simulation/Attacker.java
+++ src/main/java/Futsal_The_Simulation/Attacker.java
@@ -1,5 +1,7 @@
 package Futsal_The_Simulation;
 
+import java.util.Random;
+
 public class Attacker extends Player {
 
     Attacker(Teams myTeam, int mySector, boolean amIOnTheBall, int shootingStat, int passingStat, int defendingStat) {
@@ -10,5 +12,32 @@ public class Attacker extends Player {
         this.shootingStat = shootingStat;
         this.passingStat = passingStat;
     }
+
+    public boolean checkIfImInPositionToShoot(FieldGenerator field) {
+        switch (getMyTeam()) {
+            case FC_LEFT:
+                if (getMySector() > 20 && !field.getAllSectors().get(getMySector() - 1).getIsPlayerRightHere()) {
+                    return true;
+                }
+                break;
+            case AS_RIGHT:
+                if (getMySector() < 11 && !field.getAllSectors().get(getMySector() - 1).getIsPlayerLeftHere()) {
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+
+    public void Shoot(FieldGenerator field) {
+        if (amIOnTheBall) {
+            Random randomGenerator = new Random();
+            if (checkIfImInPositionToShoot(field)){
+                int willISucceed = shootingStat + randomGenerator.nextInt(100);
+            }
+
+        }
+    }
+
 }
 
